4947e51c91646b69fd05efe5eafb2e554f76e2c5
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2013-03-16 23:06:17 -0300

    events/GameEvent Adapted

---
+++ src/realtimelib/events/GameEvent.hx
@@ -0,0 +1,25 @@
+package realtimelib.events;
+
+import flash.events.Event;
+
+/**
+ * Game status event
+ */
+public class GameEvent extends Event
+{
+	public static const START_GAME:String = "startGame";
+	public static const GAME_OVER:String = "gameOver";
+	public static const PLAYER_READY:String = "playerReady";
+	public static const PLAYER_OUT:String = "playerOut";
+	
+	public var playerID:String;
+	public var details:Dynamic;
+	
+	public function GameEvent(type:String, playerID:String, details:Dynamic=null, bubbles:Bool=false, cancelable:Bool=false)
+	{
+		super(type, bubbles, cancelable);
+		
+		this.playerID = playerID;
+		this.details = details;
+	}
+}
