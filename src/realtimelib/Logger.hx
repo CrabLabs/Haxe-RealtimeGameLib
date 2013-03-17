1c04b615b4bb82782756f7869a60ca112b0a32e4
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2013-03-16 23:29:04 -0300

    Logger and RealtimeChannel Adapted

---
+++ src/realtimelib/Logger.hx
@@ -0,0 +1,40 @@
+/* 
+Created by Tom Krcha (http://flashrealtime.com/, http://twitter.com/tomkrcha). 
+Provided "as is" in public domain with no guarantees 
+Adapted to HaXe by CrabLabs. March 2013.
+*/
+package realtimelib;
+
+import flash.text.TextField;
+import flash.utils.getTimer;
+
+/**
+ * Logger class
+ * define txtArea and display logs
+ */
+class Logger
+{
+	
+	public static var txtArea:TextField;
+	
+	public function Logger()
+	{
+	}
+	
+	public static function log(str:String):Void
+	{
+		//trace(str);
+		if(txtArea!=null){
+			txtArea.appendText(getTimer()+": "+str+"\n");
+			txtArea.scrollV = txtArea.maxScrollV;
+		}
+	}
+	
+	public static function write(str:String):Void
+	{
+		if(txtArea!=null){
+			txtArea.appendText(str);
+			txtArea.scrollV = txtArea.maxScrollV;
+		}
+	}
+}
