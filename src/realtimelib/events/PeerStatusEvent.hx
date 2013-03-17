c6a3e7fec859552763424bb72c0b290bf3b9f8a5
Rodrigo Espinosa <espinosacurbelo@gmail.com>
2013-03-16 23:10:41 -0300

    PeerStatusEvent and StatusInfoEvent Adapted
    ConnectionStatusEvent fixed

---
+++ src/realtimelib/events/PeerStatusEvent.hx
@@ -0,0 +1,75 @@
+package realtimelib.events
+
+import flash.events.Event;
+
+class PeerStatusEvent extends Event
+{
+	// constants
+	/**
+	 * 
+	 */		
+	public static const CONNECTED:String 		= "connected";
+	
+	/**
+	 * 
+	 */
+	public static const USER_ADDED:String		= "userAdded";
+	
+	/**
+	 * 
+	 */		
+	public static const USER_REMOVED:String		= "userRemoved";
+
+	/**
+	 * 
+	 */
+	public static const USER_IDLE:String		= "userIdle";
+	
+	// state
+	private var m_info:Object;
+
+	/**
+	 *  
+	 * @param type
+	 * @param bubbles
+	 * @param cancelable
+	 * @param info
+	 * 
+	 */		
+	public function PeerStatusEvent(type:String, 
+								  bubbles:Bool=false, 
+								  cancelable:Bool=false, 
+								  info:Dynamic=null)
+	{
+		super(type, bubbles, cancelable);
+		m_info = info;
+	}
+
+	/**
+	 * 
+	 * @return 
+	 * 
+	 */		
+	public override function clone():Event
+	{
+		return new PeerStatusEvent(type, bubbles, cancelable, m_info);
+	}
+
+	/**
+	 * 
+	 * @return 
+	 * 
+	 */		
+	public override function toString():String
+	{
+		return formatToString("PeerStatusEvent", "type", "bubbles", "cancelable", "eventPhase", "info");
+	}
+
+	/**
+	 * 
+	 * @return 
+	 * 
+	 */		
+	public function get info():Dynamic			{ return m_info; }
+	public function set info(value:Object):Void	{ m_info=value; }
+}
---
+++ src/realtimelib/events/StatusInfoEvent.hx
@@ -0,0 +1,22 @@
+/* 
+Created by Tom Krcha (http://flashrealtime.com/, http://twitter.com/tomkrcha). 
+Provided "as is" in public domain with no guarantees 
+Adapted to HaXe by CrabLabs. March 2013.
+*/
+package realtimelib.events;
+
+import flash.events.Event;
+
+class StatusInfoEvent extends Event
+{
+	public static const STATUS_INFO:String = "statusInfo";
+
+	public var message:String;
+	
+	public function StatusInfoEvent(type:String, message:String, bubbles:Bool=false, cancelable:Bool=false)
+	{
+		super(type, bubbles, cancelable);
+	
+		this.message = message;
+	}
+}
